29a76bc004b5a23c7fa84ad95cc10b1850417b61
danale <danale@danales-MacBook-Pro.local>
2018-04-28 14:45:01 -0400

    Finalized project with layout animation utilizing lifecycle method

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
@@ -16,6 +34,7 @@ class ListItem extends Component {
             // has a prop called library and library has prop called title
             <Text style={titleStyle}>{title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -29,4 +48,9 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
